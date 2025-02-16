43dd0c1b7f1e10003b63e85853a52826f81aadf5
Chiukwumezie Stanley <ukadikestanley@gmail.com>
2025-02-16 23:18:08 +0000

    Start webhook inplmentation

--- includes/class-wc-gateway-squad.php
+++ includes/class-wc-gateway-squad.php
@@ -1186,11 +1186,15 @@ class WC_Gateway_Squad extends WC_Payment_Gateway_CC{
 
 		$json = file_get_contents( 'php://input' );
 
-		if( $_SERVER['HTTP_X_SQUAD_ENCRYPTED_BODY'] !== hash_hmac('sha512', $json, $this->secret_key ) ){
-			exit;
-		}
+		// if( $_SERVER['HTTP_X_SQUAD_ENCRYPTED_BODY'] !== hash_hmac('sha512', $json, $this->secret_key ) ){
+		// 	exit;
+		// }
 
 		$event = json_decode( $json );
+
+		error_log(print_r($_SERVER['HTTP_X_SQUAD_ENCRYPTED_BODY'], true));
+
+		error_log(print_r(hash_hmac('sha512', $json, $this->secret_key ), true));
 		
 		error_log(print_r($json, true));
 
